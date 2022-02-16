ea3b3fb0b5cc6b85b28057bae8ba4ab9258beb36
Daniel Escalona <daniel07escalona@gmail.com>
2022-02-16 14:47:17 -0500

    commit tree 9ba8c6960f34bf834ea862cd39c3c42caf3a3adb

--- index.js
+++ index.js
@@ -8,6 +8,73 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+ function createEmployeeRecord(employee){
+    const worker = {
+      firstName: employee[0],
+      familyName: employee[1],
+      title: employee[2],
+      payPerHour: employee[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+    return worker
+  }
+  
+  function createEmployeeRecords(employees){
+    const groupOfWorkers = []
+  
+    for( let person of employees){
+      groupOfWorkers.push(createEmployeeRecord(person))
+    }
+  
+    return groupOfWorkers
+  }
+  
+  function createTimeInEvent(date){
+    const splitDate = date.split(' ')
+  
+    const clockIn = {
+      type: "TimeIn",
+      hour: parseInt(splitDate[1]),
+      date: splitDate[0]
+    }
+  
+    this.timeInEvents.push(clockIn)
+    return this
+  }
+  
+  function createTimeOutEvent(date){
+    const splitDate = date.split(' ')
+  
+    const clockOut = {
+      type: "TimeOut",
+      hour: parseInt(splitDate[1]),
+      date: splitDate[0]
+    }
+  
+    this.timeOutEvents.push(clockOut)
+    return this
+  }
+  
+  function hoursWorkedOnDate(date){
+    let timeIn = this.timeInEvents.find( elem => elem.date === date)
+    let timeOut = this.timeOutEvents.find( elem => elem.date === date )
+    return((timeOut.hour-timeIn.hour)/100)
+  }
+  
+  function wagesEarnedOnDate(date){
+    let hoursWorked = hoursWorkedOnDate.call(this, date)
+    return hoursWorked*this.payPerHour
+  
+  }
+  
+  function calculatePayroll(employees){
+    const totalPayRoll = employees.reduce((accumulator, currentValue) => {
+        return accumulator+allWagesFor.call(currentValue)
+    }, 0)
+    return totalPayRoll
+  }
+
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -21,3 +88,6 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(employees, firstName){
+    return employees.find( elem => elem.firstName === firstName)
+}
